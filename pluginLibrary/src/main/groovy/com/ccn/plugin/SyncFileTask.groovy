b3034ba2b8a296aa27a11d5ff64ad5717b6c4ab7
gaochun <jz_gaochun@163.com>
2021-11-04 17:39:05 +0800

    update.

--- pluginLibrary/src/main/groovy/com/ccn/plugin/SyncFileTask.groovy
+++ pluginLibrary/src/main/groovy/com/ccn/plugin/SyncFileTask.groovy
@@ -6,26 +6,39 @@ import org.gradle.api.Task
 
 class SyncFileTask implements Plugin<Project> {
 
-    void apply(Project target) {
+    void apply(Project project) {
+
+        project.task('hello') {
+            group = "pda"
+            description = "gradle build script demo,shares only in this build.gradle"
+            doLast {
+                println "Hello from the BuildScriptPlugin"
+                println '--------------------------------->' + System.getProperty("user.dir")
+                println '--------------------------------->' + project.rootProject.name
+                println '--------------------------------->' + project.rootProject.buildFile
+            }
+        }
+
+
         //def rootProjectPath = rootProject.projectDir.path
         println '--------------------------------->' + System.getProperty("user.dir")
         //println '--------------------------------->' +new File(System.getProperty("user.dir")).name
         //println '--------------------------------->' +new File(System.getProperty("user.dir")).parent
         //println '--------------------------------->' + new SystemProperties().properties
 
-
         //def debugBuildConfigFieldName = android.properties.get('buildTypes').getByName("debug").getBuildConfigFields().APP_TYPE.getValue().replaceAll("\"", "")
         //println 'buildTypes debug APP_TYPE : --------------------------------->' + debugBuildConfigFieldName
-        A_PushXmlToPDA("QiaoPai", target)
+        A_PushXmlToPDA("QiaoPai", project)
+
 
     }
 
-    void A_PushXmlToPDA(def projectName, Project target) {
+    void A_PushXmlToPDA(def projectName, Project project) {
         def rootProjectPath = System.getProperty("user.dir") + File.separator + 'AllProject' + File.separator + projectName + File.separator
         def PDASDCardPath = ' /sdcard/防伪追溯/'
-        println '--------------------------------->' + rootProjectPath
+        //println '--------------------------------->' + rootProjectPath
         //注：为了保持开发的xml目录和PDA中保持一致，会删除PDA中Configuration目录，再进行推送，推送时会包含AllProject中具体项目下的CNG.xml
-        Task deleteConfigurationDir = target.task("deleteConfigurationDir") {
+        Task deleteConfigurationDir = project.task("deleteConfigurationDir") {
             doLast {
                 //def devicePath = ' /sdcard/防伪追溯/'
                 def deleteConfigurationDirCommand = 'adb shell cd /sdcard && rm -r ' + PDASDCardPath + 'Configuration'
@@ -37,7 +50,7 @@ class SyncFileTask implements Plugin<Project> {
             }
         }
 
-        target.task("A_PushXmlToPDA", dependsOn: deleteConfigurationDir) {
+        project.task("A_PushXmlToPDA", dependsOn: deleteConfigurationDir) {
             group = "PDA"
             description = "卸载App并删除PDA本地防伪追溯目录"
             doLast {
@@ -45,7 +58,7 @@ class SyncFileTask implements Plugin<Project> {
                 //自定义的XML文件路径（可按具体存放位置更改）
                 //def fromPath = project.projectDir.path + File.separator + 'XML' + File.separator + projectName + File.separator + 'Configuration'
 
-                /*通过执行adb命令，将文件push到指定目录*/
+                //通过执行adb命令，将文件push到指定目录
                 //def adbCommand='adb push D:/0000.txt /sdcard/防伪追溯'
                 def fromPath = rootProjectPath + 'Configuration'
                 println "fromPath----> " + fromPath
@@ -75,10 +88,7 @@ class SyncFileTask implements Plugin<Project> {
                 }
             }
         }
-        //tasks.A_PushXmlToPDA.dependsOn(deleteConfigurationDir)
-
     }
-
 
 }
 
